13260a33889a6fb3bda0bde0ce3c6268000d4271
Yaroalibou <abdilkadirhalilu01@gmail.com>
2025-12-11 00:34:02 +0000

    Update Firebase-config.js

--- Firebase-config.js
+++ Firebase-config.js
@@ -1,57 +1,20 @@
-// Your Firebase Configuration
-const firebaseConfig = {
-  "auth": {
-    "uid": "1f949b0b-1d31-44e9-ae10-d03369147f3d",
-    "token": {
-      "sub": "1f949b0b-1d31-44e9-ae10-d03369147f3d",
-      "firebase": {
-        "sign_in_provider": "google.com"
-      },
-      "email": "Mdlaures@gmail.com",
-      "email_verified": true,
-      "phone_number": "+233593416186",
-      "name": "Yaro"
-    }
-  },
-  "resource": {
-    "key": "value"
-  },
-  "path": "/phonelockcontroller",
-  "method": "set",
-  "time": "2025-12-10T13:27:16.353Z",
-  "isAdmin": false
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-try {
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-} catch (error) {
-  console.error("Firebase initialization error:", error);
-}
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB9h_WzWufSD5gOIF6Y1-j2-ss0tFT3ry8",
+    authDomain: "phonelockcontroller.firebaseapp.com",
+    databaseURL: "https://phonelockcontroller-default-rtdb.firebaseio.com",
+    projectId: "phonelockcontroller",
+    storageBucket: "phonelockcontroller.firebasestorage.app",
+    messagingSenderId: "622974989562",
+    appId: "1:622974989562:web:4799df0c2cc5d5a1c54543"
+  };
 
-// Firebase Services
-const auth = firebase.auth();
-const database = firebase.database();
-const storage = firebase.storage();
-
-// Authentication State Listener
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    console.log("User signed in:", user.uid);
-    if (typeof onAuthStateChanged === 'function') {
-      onAuthStateChanged(user);
-    }
-  } else {
-    console.log("No user signed in");
-    // Sign in anonymously
-    auth.signInAnonymously()
-      .catch(error => console.error("Auth error:", error));
-  }
-});
-
-// Export services
-window.firebaseAuth = auth;
-window.firebaseDatabase = database;
-window.firebaseStorage = storage;
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
